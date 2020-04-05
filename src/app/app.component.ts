d6743b0a16d1bea27e2290c3e1ed6f2446b3e424
Vladimir Vysokomornyi <unlimit_rmc@mail.ru>
2020-04-05 17:11:08 +0200

    [Pipes] 246 Add a filter pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -32,6 +32,7 @@ export class AppComponent {
       started: new Date(2017, 1, 15)
     }
   ];
+  filteredStatus: string = '';
 
   getStatusClasses(server: { instanceType: string, name: string, status: string, started: Date }) {
     return {
